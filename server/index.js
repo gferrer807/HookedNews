69b333909fba1a5eccc13ee32f35d8a9340c1269
gferrer807 <gf807@nyu.edu>
2019-05-02 15:14:29 -0400

    hooked site

--- server/index.js
+++ server/index.js
@@ -26,6 +26,6 @@ app.get('/tickers', (req, res) => {
 })
 
 
-app.listen(3000, () => {
-    console.log('listening on port 3000')
+app.listen(80, () => {
+    console.log('listening on port 80')
 })
